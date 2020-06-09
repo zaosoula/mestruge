aa749a79b8def6dfdc1b31cf2486d149051b160e
Zao Soula <contact@zaosoula.fr>
2020-06-09 17:34:39 +0200

    1.0.9

--- pages/html/cottages.php
+++ pages/html/cottages.php
@@ -1,4 +1,4 @@
-<section class="site-hero inner-page overlay" style="background-image: url(<?php echo BaseUrl; ?>/assets/images/hero_4.jpg)" data-stellar-background-ratio="0.5">
+<section class="site-hero overlay" style="background-image: url(<?php echo BaseUrl; ?>/assets/images/hero_4.jpg)" data-stellar-background-ratio="0.5">
   <div class="container">
     <div class="row site-hero-inner justify-content-center align-items-center">
       <div class="col-md-10 text-center" data-aos="fade">
@@ -10,7 +10,7 @@
         </ul>
       </div>
     </div>
-  </div>  
+  </div>
   <a class="mouse smoothscroll" href="#next">
     <div class="mouse-icon">
       <span class="mouse-wheel"></span>
--- pages/html/garden.php
+++ pages/html/garden.php
@@ -1,4 +1,4 @@
-<section class="site-hero inner-page overlay" style="background-image: url(<?php $__('gardenHeroBackground'); ?>)" data-stellar-background-ratio="0.5">
+<section class="site-hero overlay" style="background-image: url(<?php $__('gardenHeroBackground'); ?>)" data-stellar-background-ratio="0.5">
   <div class="container">
     <div class="row site-hero-inner justify-content-center align-items-center">
       <div class="col-md-10 text-center" data-aos="fade">
